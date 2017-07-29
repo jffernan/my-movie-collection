3788ba5b7543e6736283fab2b65e769c9816d877
James Fernandez <jffernan@comcast.net>
2017-07-29 19:48:21 +0000

    created Rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-1.	require 'bundler'
-2.	Bundler.require
-3.	ActiveRecord::Base.establish_connection(
-4.	:adapter => "sqlite3",
-5.	:database => "db/development.sqlite"
-6.	)
-7.	require_all 'app'
+require 'bundler'
+Bundler.require
+ActiveRecord::Base.establish_connection(
+:adapter => "sqlite3",
+:database => "db/development.sqlite"
+)
+require_all 'app'
