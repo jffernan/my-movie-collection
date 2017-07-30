52ba64609cfa5b1606790a997d48592a2459039b
James Fernandez <jffernan@comcast.net>
2017-07-30 18:53:51 +0000

    2 minor updates to show date format XX/XX/XXXX and comment 1 movie bought owned by 1 user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user #1 Movie is owned (bought) by 1 User
 
   def self.alphabetical_order #ActiveRecord method to alphabetize lists
     Movie.order(title: :asc)
