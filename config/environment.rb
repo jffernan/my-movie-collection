2f0aa2a97421dddc3c8acdd4250059f0042b65a1
jffernan <jffernan@comcast.net>
2018-04-12 15:49:46 -0700

    added link to view code on Github

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 ActiveRecord::Base.establish_connection(
-:adapter => "postgresql",
-:database => "config/database.yml"
+  :adapter => "postgresql",
+  :database => "config/database.yml"
 )
 require_all 'app'
